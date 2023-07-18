88aa05de8cd4cb701e183bb98573eaff145e7341
Adam Letts <adam@stability.ai>
2023-07-18 03:31:11 +0000

    Improve organization of CPCargo integration

--- launch.py
+++ launch.py
@@ -3,35 +3,9 @@ import contextlib
 import logging
 import os, signal
 import sys
+from threestudio.utils.cpcargo import CPCargoCheckpointing
 
-sys.path.append("CPCargo/src") # use local run_cfg (from same directory)
-
-from CPCargo import CheckpointCargo, Heartbeat
-
-SignalCheckpoint = False
 logger = logging.getLogger("pytorch_lightning")
-
-import pytorch_lightning
-from threestudio.utils.misc import parse_version
-
-if parse_version(pytorch_lightning.__version__) > parse_version("1.8"):
-    from pytorch_lightning.callbacks import Callback
-else:
-    from pytorch_lightning.callbacks.base import Callback
-
-from pytorch_lightning.callbacks.progress import TQDMProgressBar
-from pytorch_lightning.utilities.rank_zero import rank_zero_only, rank_zero_warn
-
-cpcargo_heartbeat = None
-
-class HeartbeatCallback(Callback):
-    def __init__(self, timeout):
-        super().__init__()
-        self.hb = Heartbeat(timeout=timeout)
-
-    @rank_zero_only
-    def on_train_batch_start(self, trainer, pl_module, *args, **kwargs):
-        self.hb.pulse()
 
 class ColoredFilter(logging.Filter):
     """
@@ -65,14 +39,6 @@ class ColoredFilter(logging.Filter):
             record.levelname = f"{color_start}[{record.levelname}]"
             record.msg = f"{record.msg}{self.RESET}"
         return True
-
-def signal_handler(signum, frame):
-  # trigger a checkpoint and then exit cleanly
-  # do kill -15 <pid> from a separate shell to trigger checkpoint-then-exit behavior
-  logger.info("Got signal {sig}".format(sig=signal.Signals(signum).name))
-  if signum == signal.SIGTERM:
-    global SignalCheckpoint
-    SignalCheckpoint = True
 
 def main(args, extras) -> None:
     global SignalCheckpoint
@@ -155,21 +121,8 @@ def main(args, extras) -> None:
     if args.train:
         ckpts_dir = os.path.join(cfg.trial_dir, "ckpts")
 
-        # Set up saving to S3 via CPCargo
-        # Set a signal handler for sigterm to capture termination request
-        signal.signal(signal.SIGTERM, signal_handler)
-        os.makedirs(ckpts_dir, exist_ok = False) 
-        checkpoint_path = ckpts_dir
-        dest_s3_path = f"s3://{args.dest_s3bucket}/threestudio-ckpts"
-        CP = CheckpointCargo(src_dir=checkpoint_path,
-                            dst_url=dest_s3_path,
-                            region=args.region,
-                            file_regex=r'.*',
-                            recursive=True)
-        
-        # You can start monitoring checkpoint directory after data pipeline subprocesses forks
-        CP.start()
-        callbacks += [HeartbeatCallback(8)]
+        cpcargo_timeout = 8
+        cpcargo_checkpointing = CPCargoCheckpointing(args.region, args.dest_s3bucket, ckpts_dir, cpcargo_timeout, callbacks)
 
         callbacks += [
             ModelCheckpoint(
